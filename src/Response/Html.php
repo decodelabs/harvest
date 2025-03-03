639b636ae712393431f5b45e6a419b6c8be9b05f
Tom Wright <tom@inflatablecookie.com>
2025-03-03 15:32:25 +0000

    Added closure and generator support to response factories
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Response/Html.php
+++ src/Response/Html.php
@@ -9,23 +9,32 @@ declare(strict_types=1);
 
 namespace DecodeLabs\Harvest\Response;
 
+use Closure;
 use DecodeLabs\Harvest\Message\Stream as MessageStream;
+use DecodeLabs\Harvest\Message\StringableToStringTrait;
+use Generator;
 use Stringable;
 
 class Html extends Stream
 {
+    use StringableToStringTrait;
+
     /**
      * Init with HTML stream and content type headers set
      *
-     * @param array<string, string|Stringable|array<string|Stringable>> $headers
+     * @param string|Stringable|Generator<string|Stringable>|Closure(self=):(string|Stringable|Generator<string|Stringable>) $html
+     * @param array<string,string|Stringable|array<string|Stringable>> $headers
      */
     public function __construct(
-        string $html,
+        string|Stringable|Generator|Closure $html,
         int $status = 200,
         array $headers = []
     ) {
         parent::__construct(
-            MessageStream::fromString($html, 'wb+'),
+            MessageStream::fromString(
+                content: self::stringableToString($html, $this),
+                mode: 'wb+'
+            ),
             $status,
             $this->injectDefaultHeaders([
                 'content-type' => 'text/html; charset=utf-8',
--- stubs/DecodeLabs/Harvest.php
+++ stubs/DecodeLabs/Harvest.php
@@ -15,16 +15,18 @@ use DecodeLabs\Harvest\Request as Ref3;
 use DecodeLabs\Harvest\Transport as Ref4;
 use DecodeLabs\Deliverance\Channel\Stream as Ref5;
 use DecodeLabs\Harvest\Response\Stream as Ref6;
-use DecodeLabs\Harvest\Response\Text as Ref7;
-use DecodeLabs\Harvest\Response\Html as Ref8;
-use DecodeLabs\Harvest\Response\Json as Ref9;
-use DecodeLabs\Harvest\Response\Xml as Ref10;
-use Psr\Http\Message\UriInterface as Ref11;
-use DecodeLabs\Harvest\Response\Redirect as Ref12;
-use Traversable as Ref13;
-use Closure as Ref14;
-use Psr\Http\Message\ServerRequestInterface as Ref15;
-use DecodeLabs\Compass\Ip as Ref16;
+use Stringable as Ref7;
+use Generator as Ref8;
+use Closure as Ref9;
+use DecodeLabs\Harvest\Response\Text as Ref10;
+use DecodeLabs\Harvest\Response\Html as Ref11;
+use DecodeLabs\Harvest\Response\Json as Ref12;
+use DecodeLabs\Harvest\Response\Xml as Ref13;
+use Psr\Http\Message\UriInterface as Ref14;
+use DecodeLabs\Harvest\Response\Redirect as Ref15;
+use Traversable as Ref16;
+use Psr\Http\Message\ServerRequestInterface as Ref17;
+use DecodeLabs\Compass\Ip as Ref18;
 
 class Harvest implements Proxy
 {
@@ -56,28 +58,28 @@ class Harvest implements Proxy
     public static function stream(Ref5|Ref1|string $body = 'php://memory', int $status = 200, array $headers = []): Ref6 {
         return static::$_veneerInstance->stream(...func_get_args());
     }
-    public static function text(string $text, int $status = 200, array $headers = []): Ref7 {
+    public static function text(Ref7|Ref8|Ref9|string $text, int $status = 200, array $headers = []): Ref10 {
         return static::$_veneerInstance->text(...func_get_args());
     }
-    public static function html(string $html, int $status = 200, array $headers = []): Ref8 {
+    public static function html(Ref7|Ref8|Ref9|string $html, int $status = 200, array $headers = []): Ref11 {
         return static::$_veneerInstance->html(...func_get_args());
     }
-    public static function json(mixed $data, int $status = 200, array $headers = []): Ref9 {
+    public static function json(mixed $data, int $status = 200, array $headers = []): Ref12 {
         return static::$_veneerInstance->json(...func_get_args());
     }
-    public static function xml(string $xml, int $status = 200, array $headers = []): Ref10 {
+    public static function xml(Ref7|Ref8|Ref9|string $xml, int $status = 200, array $headers = []): Ref13 {
         return static::$_veneerInstance->xml(...func_get_args());
     }
-    public static function redirect(Ref11|string $uri, int $status = 302, array $headers = []): Ref12 {
+    public static function redirect(Ref14|string $uri, int $status = 302, array $headers = []): Ref15 {
         return static::$_veneerInstance->redirect(...func_get_args());
     }
-    public static function generator(Ref13|Ref14|array $iterator, int $status = 200, array $headers = []): Ref6 {
+    public static function generator(Ref16|Ref9|array $iterator, int $status = 200, array $headers = []): Ref6 {
         return static::$_veneerInstance->generator(...func_get_args());
     }
-    public static function liveGenerator(Ref13|Ref14|array $iterator, int $status = 200, array $headers = []): Ref6 {
+    public static function liveGenerator(Ref16|Ref9|array $iterator, int $status = 200, array $headers = []): Ref6 {
         return static::$_veneerInstance->liveGenerator(...func_get_args());
     }
-    public static function extractIpFromRequest(Ref15 $request): Ref16 {
+    public static function extractIpFromRequest(Ref17 $request): Ref18 {
         return static::$_veneerInstance->extractIpFromRequest(...func_get_args());
     }
 };
